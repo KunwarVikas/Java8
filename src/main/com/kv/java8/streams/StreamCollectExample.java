d3032d3b26d2c75dbccf98e9da31238858afd0f6
Kunwar <kunwar.nitw@gmail.com>
2020-08-20 23:49:42 +0530

    Streams Example

--- src/main/com/kv/java8/streams/StreamCollectExample.java
+++ src/main/com/kv/java8/streams/StreamCollectExample.java
@@ -8,14 +8,17 @@ import java.util.stream.Stream;
 
 public class StreamCollectExample {
     public static void main(String[] args) {
+        //list
         Stream<Integer> intStream = Stream.of(1,2,3,4,5);
         List<Integer> intList = intStream.collect(Collectors.toList());
         System.out.println(intList); //prints [1, 2, 3, 4, 5]
 
+        //map
         intStream = Stream.of(1,2,3,4); //stream is closed, so we need to create it again
         Map<Integer,Integer> intMap = intStream.collect(Collectors.toMap(i -> i, i -> i+10));
         System.out.println(intMap);
 
+        //array
         Stream<Integer> intStream1 = Stream.of(1,2,3,4);
         Integer[] intArray = intStream1.toArray(Integer[]::new);
         System.out.println(Arrays.toString(intArray)); //prints [1, 2, 3, 4]
