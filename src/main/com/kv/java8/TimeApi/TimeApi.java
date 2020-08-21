3d5f46ab994159fb19bbf5dfd644f9ad4d5d4e1a
Kunwar <kunwar.nitw@gmail.com>
2020-08-22 00:33:23 +0530

    Streams Examples

---
+++ src/main/com/kv/java8/TimeApi/TimeApi.java
@@ -0,0 +1,34 @@
+package main.com.kv.java8.TimeApi;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.time.ZoneId;
+
+/**
+ * LocalDate()
+ */
+public class TimeApi {
+    public static void main(String[] args) {
+        LocalDate date = LocalDate.now();
+        System.out.println("Current Date="+date);
+
+        //Creating LocalDate by providing input arguments
+        LocalDate firstDay_2020 = LocalDate.of(2020, Month.JANUARY, 1);
+        System.out.println("Specific Date="+firstDay_2020);
+
+        //Current date in "Asia/Kolkata", you can get it from ZoneId javadoc
+        LocalDate todayKolkata = LocalDate.now(ZoneId.of("Asia/Kolkata"));
+        System.out.println("Current Date in IST="+todayKolkata);
+
+        //java.time.zone.ZoneRulesException: Unknown time-zone ID: IST
+        //LocalDate todayIST = LocalDate.now(ZoneId.of("IST"));
+
+        //Getting date from the base date i.e 01/01/1970
+        LocalDate dateFromBase = LocalDate.ofEpochDay(365);
+        System.out.println("365th day from base date= "+dateFromBase);
+
+        LocalDate hundredDay2014 = LocalDate.ofYearDay(2014, 100);
+        System.out.println("100th day of 2014="+hundredDay2014);
+    }
+
+}
--- src/main/com/kv/java8/streams/StreamTerminalOps.java
+++ src/main/com/kv/java8/streams/StreamTerminalOps.java
@@ -1,4 +1,56 @@
 package main.com.kv.java8.streams;
 
+import java.util.Optional;
+import java.util.stream.Stream;
+
+/**
+ * reduce()
+ * count()
+ * match()
+ * findFirst()
+ * anyMatch ()
+ * allMatch()
+ * noneMatch()
+ */
 public class StreamTerminalOps {
+    public static void main(String[] args) {
+        //reduce
+        // We can use reduce() to perform a reduction on the elements of the stream,
+        // using an associative accumulation function,
+        // and return an Optional. Let’s see how we can use it multiply the integers in a stream.
+        Stream<Integer> numbers = Stream.of(1,2,3,4,5);
+        Optional<Integer> reduce = numbers.reduce((i, j) -> {
+            return i * j;
+        });
+        if(reduce.isPresent()){
+            System.out.println(reduce.get());
+        }
+
+        //count
+        Stream<Integer> numbers1 = Stream.of(1,2,3,4,5);
+        long count = numbers1.count();
+        System.out.println(count);
+
+        //anyMatch, allMatch, noneMatch
+        Stream<Integer> numbers3 = Stream.of(1,2,3,4,5);
+        System.out.println("Stream contains 4? "+numbers3.anyMatch(i -> i==4));
+        //Stream contains 4? true
+
+        Stream<Integer> numbers4 = Stream.of(1,2,3,4,5);
+        System.out.println("Stream contains all elements less than 10? "+numbers4.allMatch(i -> i<10));
+        //Stream contains all elements less than 10? true
+
+        Stream<Integer> numbers5 = Stream.of(1,2,3,4,5);
+        System.out.println("Stream doesn't contain 10? "+ numbers5.noneMatch(i -> i==10));
+        //Stream doesn't contain 10? true
+
+        //circuiting terminal ops
+        Stream<String> names4 = Stream.of("amit","ajit","David", "Lisa");
+        Optional<String> firstNameWithD = names4.filter(i -> i.startsWith("D")).findFirst();
+        if(firstNameWithD.isPresent()){
+            System.out.println("First Name starting with D="+firstNameWithD.get()); //David
+        }
+
+
+    }
 }
